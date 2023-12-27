ec15f05ab273ccf6ad7c065739bd727ee9718bc9
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2023-12-27 13:22:08 +0000

    Update

---
+++ components/collectionPage.jsx
@@ -0,0 +1,24 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { getPagesUnderRoute } from 'nextra/context'
+import filterRouteLocale from 'nextra/filter-route-locale'
+
+export function CollectionPage({ path }) {
+  const { locale, defaultLocale } = useRouter()
+  let result = filterRouteLocale(
+    getPagesUnderRoute(path),
+    locale, defaultLocale
+  ).map(page => {
+      // console.log(page.frontMatter?.title)
+      return (
+        <li key={page.route}>
+          <Link href={page.route}>{page.meta.title || page.frontMatter?.title || page.name}</Link>
+        </li>
+       )
+    })
+    return (
+        <ol>
+            {result}
+        </ol>
+    )
+}
