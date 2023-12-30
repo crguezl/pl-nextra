9dcc38434e3f090999b0fbcf240fb522b7c72fd9
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2023-12-30 09:35:04 +0000

    learning nextra

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
-const withNextra = require('nextra')({
+const myConfig = {
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
   latex: true
-})
+}
+
+const withNextra = require('nextra')(myConfig)
 
 module.exports = withNextra()
