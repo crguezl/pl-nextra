1a3f0bc72b721b70fe33c6059fff6c4ed13fe455
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2024-01-01 21:30:06 +0000

    Update

--- src/auth.js
+++ src/auth.js
@@ -5,11 +5,12 @@ import GithubProvider from "next-auth/providers/github"
 // You'll need to import and pass this
 // to `NextAuth` in `app/api/auth/[...nextauth]/route.ts`
 export const config = {
+  debug: true,
   providers: [
     GithubProvider({
         clientId: process.env.GITHUB_ID,
         clientSecret: process.env.GITHUB_SECRET,
-        allowDangerousEmailAccountLinking: true // https://next-auth.js.org/configuration/providers/oauth#allowdangerousemailaccountlinking-option
+        //allowDangerousEmailAccountLinking: true // https://next-auth.js.org/configuration/providers/oauth#allowdangerousemailaccountlinking-option
       }),
     /*
     TwitterProvider({
