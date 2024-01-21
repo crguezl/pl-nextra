2112322f8328421da1e9ceae75caa82530ebf0c8
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2024-01-21 14:04:29 +0000

    Update

---
+++ components/related-labs.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'nextra-theme-docs';
+import styles from './counters.module.css'
+
+export default function relatedLabs({ labs} ) {
+  let labsJSX = labs.map(lab => {
+        return <li><Link href={`/labs/${lab}.html`} key={lab}>{lab}</Link></li>
+  })
+  return (
+    <ul className={ styles.myList}>
+        {labsJSX}
+    </ul>
+  )
+}
