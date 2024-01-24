aad814d6675a0b6a2acd58edb3863e8ee261dbdb
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2024-01-24 21:15:32 +0000

    Update

--- components/teams.jsx
+++ components/teams.jsx
@@ -1,21 +1,22 @@
 import styles from './counters.module.css'
 import useSWR from 'swr'
 import { Link } from 'nextra-theme-docs';
- 
+
 const fetcher = (...args) => fetch(...args).then((res) => res.json())
- 
+
 export default function Teams() {
-  const { data, error } = useSWR('/api/getteams', fetcher)
- 
-  if (error) return <div>Failed to load</div>
-  if (!data) return <div>Loading...</div>
- 
-  return (
-    <div>
-      {(data.teams.length > 1)? (<p>{data.teams.length} Teams</p>):('')}
-      <ul className={styles.uList}>
-        { data.teams.map(t => (<li key={t.name}><Link href={t.url}>{t.name}</Link></li>)) }
-      </ul>
-      </div>
-  )
+    const { data, error } = useSWR('/api/getteams', fetcher)
+
+    if (error) return <div>Failed to load</div>
+    if (!data) return <div>Loading...</div>
+
+    if (data?.teams?.length === 0) return (
+        <div>
+            <p>{data.teams.length} Teams</p>)
+            <ul className={styles.uList}>
+                {data.teams.map(t => (<li key={t.name}><Link href={t.url}>{t.name}</Link></li>))}
+            </ul>
+        </div>
+    )
+    return 'Sign in to see your teams';
 }
