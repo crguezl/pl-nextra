5ed3521bf1e8a9795886f421ac23498fc3bf2051
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2024-01-29 10:22:54 +0000

    Update

--- components/rubric.jsx
+++ components/rubric.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import styles from './counters.module.css'
 
 export default function Rubrica({ rubric }) {
     let lista = rubric.map((item, index) => {
         return (
           <div key={index}>
-            <input type="checkbox" id={index} name={index} value={index} />
+            <input type="checkbox" id={index} name={index} value={index} class={styles.input}/>
             <label htmlFor={index}>{item}</label><br />
           </div>
         )
