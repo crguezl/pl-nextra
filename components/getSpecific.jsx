926d597a99fd0ca3a64b5e726153c3bd714940e8
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2024-02-26 17:14:14 +0000

    Update

---
+++ components/getSpecific.jsx
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+import { getPagesUnderRoute } from 'nextra/context'
+import filterRouteLocale from 'nextra/filter-route-locale'
+import { useRouter } from 'next/router'
+import styles from './counters.module.css'
+
+import React from 'react'
+
+export default function getSpecific({ path }) {
+    const { locale, defaultLocale } = useRouter()
+    const pages = getPagesUnderRoute(path)
+    //console.error(pages)
+    let result = filterRouteLocale(pages, locale, defaultLocale).map(page => {
+        if (page.frontMatter?.display === 'hidden') return null
+        return (
+          <li key={page.route}>
+            <Link className={styles.link} href={page.route}>{page.meta?.title || page.frontMatter?.title || page?.name}</Link> {page.frontMatter?.key} 
+          </li>
+         )
+      })
+      return (<ol className={styles.myList} reversed>{result}</ol>)
+}
