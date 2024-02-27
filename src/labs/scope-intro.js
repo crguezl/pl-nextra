ac20ac82d1252d2218369925144129e81df712cb
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2024-02-27 12:35:21 +0000

    Update

--- src/labs/scope-intro.js
+++ src/labs/scope-intro.js
@@ -1,2 +1,3 @@
-export const  videos =    [ "NfC74pLH0yA"]
-export const  videos23 = [ "kHncLcKuXLc", "2rNPRfn15Aw", "w6_JqrGxFi4"]
+export const videos = [ "NfC74pLH0yA"]
+export const videos23 = [ "kHncLcKuXLc", "2rNPRfn15Aw", "w6_JqrGxFi4"]
+export const video20240227 = [ "CM6krVgcO5E" ]
