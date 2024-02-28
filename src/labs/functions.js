fb2845e1a93b1d29fe87264f424d23fbe2a6c1ab
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2024-02-28 21:17:09 +0000

    Update

---
+++ src/labs/functions.js
@@ -0,0 +1,19 @@
+export const videos ={
+  "video20230308": {
+    "date": "08/03/2023",
+    "description": "Clase del 08/03/2023. Se describe el soporte para scopes de ast-types",
+    "id": "CR_GI81TFt0"
+  },
+  "video20230313": {
+    "date": "13/03/2023",
+    "description": "Clase del 13/03/2023. Se describe el soporte de ast-types para el recorrrido y modificación de nodos (Métodos visit)",
+    "url": "https://youtu.be/CR_GI81TFt0",
+    "id": "CR_GI81TFt0"
+  },
+  "video20230322": {
+    "date": "22/03/2023",
+    "description": "Clase del 22/03/2023. Hasta el minuto 25 se explica como realizar el examen parcial del curso 22/23. Le será muy útil para preparar el próximo examen del 11/03. A partir del minuto 25 se explican las bases para la práctica functions",
+    "url": "https://youtu.be/SfAIHgAqSuQ",
+    "id": "SfAIHgAqSuQ"
+  }
+}
