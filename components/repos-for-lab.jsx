9d7367a7462243b783fa50ef58dc9359e0fec33c
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2024-03-05 11:32:29 +0000

    Update

--- components/repos-for-lab.jsx
+++ components/repos-for-lab.jsx
@@ -1,11 +1,18 @@
 import { Link } from '@/src/frontmatter'
-
+import Styles from '@/components/counters.module.css'
 import React from 'react'
 
-export default function ReposForLab({ lab }) {
+export default function ReposForLab({ lab}) {
   return (
     <div>
-        Repos for lab <Link href={"https://github.com/orgs/ULL-ESIT-PL-2324/repositories?q="+lab}>{lab}</Link>
+      <ul className={Styles.uList}>
+        <li>Repos for lab <Link href={"https://github.com/orgs/ULL-ESIT-PL-2324/repositories?q="+lab}>{lab}</Link></li>
+        <li>
+          <Link href={"https://classroom.github.com/classrooms/153934884-ull-esit-pl-2324/assignments/"+lab}>
+          GitHub Classroom Assignments for lab {lab}
+          </Link>
+        </li>
+      </ul>
     </div>
   )
 }
