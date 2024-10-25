ca752c73a887c8142809a7733c8f8f704e6bb7c4
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2024-10-25 13:21:16 +0100

    Update

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@ const myConfig = {
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
   latex: true,
-  images: {
-    remotePatterns: [
+  images: { /* To protect your application from malicious users, configuration is required in order to use external images. This ensures that only external images from your account can be served from the Next.js Image Optimization API. These external images can be configured with the remotePatterns property in your next.config.js file */
+    remotePatterns: [ // See https://nextjs.org/docs/pages/api-reference/components/image#remotepatterns
       {
         protocol: 'https',
         hostname: 'avatars.githubusercontent.com',
