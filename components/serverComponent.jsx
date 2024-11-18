2004c51d404d5d1afa53643c7ae6904bb0c2ad48
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2024-11-18 20:45:40 +0000

    Update

---
+++ components/serverComponent.jsx
@@ -0,0 +1,33 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+
+const catURL=  "https://api.thecatapi.com/v1/images/search?size=full"
+const fetcher = () => fetch(catURL).then((res) => res.json())
+
+async function fetchData() {
+  // Simulate an API call
+  let cat = await fetcher()
+  return cat[0]
+}
+
+function DataFetcher() {
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    fetchData().then(setData)
+  }, [])
+
+  if (!data) return null
+
+  return <img src={data.url} width={data.width} height={data.height} />
+}
+
+export default function ServerComponent() {
+  return (
+    <div>
+      <h2>Server Component Example</h2>
+      <DataFetcher />
+    </div>
+  )
+}
